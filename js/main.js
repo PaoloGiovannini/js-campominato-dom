680fd4310c2dc936f2c9a265502ef638f7d9724c
Paolo Giovannini <giovannini13@gmail.com>
2023-03-06 17:08:37 +0100

    aggiuntavittoria e pointer dopo vittoria/sconfitta

--- js/main.js
+++ js/main.js
@@ -2,13 +2,13 @@ const containerDom = document.querySelector('.container');
 const bottoneDom = document.querySelector('#bottone');
 const mainDom = document.querySelector('#main');
 const footerDom = document.querySelector('#footer');
-let click = 0;
-
 
 
 bottoneDom.addEventListener('click', function() {
     const numeroBombe = 16;
     const arrayBombe = [];
+    let click = 0;
+    let punteggioDom = document.getElementById('punteggio')
 
    while (arrayBombe.length < numeroBombe){
        let numeroCasuale = (Math.floor(Math.random() * 100 + 1));
@@ -18,13 +18,14 @@ bottoneDom.addEventListener('click', function() {
        }
    }
    console.log(arrayBombe);
-    let punteggioDom = document.getElementById('punteggio')
+
     mainDom.classList.remove('d-none');
     footerDom.classList.remove('d-none');
     containerDom.innerHTML = '';
     punteggioDom.innerHTML = '';
     click='';
     mainDom.classList.remove('pointer');
+
     for (let i = 1; i <= 100; i++ ) {
         
         const quadratino = creazioneQuadrato(i);
@@ -34,19 +35,21 @@ bottoneDom.addEventListener('click', function() {
             click++
             
             this.classList.toggle('cliccato');
-            quadratino.classList.add('pointer')
+            quadratino.classList.add('pointer');
             punteggioDom.innerHTML = `Il tuo punteggio è ${click}`;
+            if(click == 84){
+                punteggioDom.innerHTML = 'HAI VINTOOOO';
+                mainDom.classList.add('pointer');
+            }
             for (let x = 0; x < arrayBombe.length; x++){
                 if (quadratino.textContent == arrayBombe[x]){
-                    event.stopPropagation();
-                    this.classList.toggle('red');
-                    punteggioDom.innerHTML = 'hai perso';
+                    quadratino.classList.toggle('red');
+                    punteggioDom.innerHTML = 'Hai perso';
                     console.log(quadratino.textContent);
                     mainDom.classList.add('pointer');
                     
                 }
             }
-            console.log(i);
         }
         );
         
